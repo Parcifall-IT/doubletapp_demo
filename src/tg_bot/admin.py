881a9f5ce76bf405fc0efe6b22ad3bc58c5683da
Чухарева Анна Алексеевна <i.chuxarev205@yandex.ru>
2025-02-12 23:32:53 +0500

    подключена бд с админом

--- src/tg_bot/admin.py
+++ src/tg_bot/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import AppAdminuser
+
 
 # Register your models here.
+@admin.register(AppAdminuser)
+class AppAdminuserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "first_name", "last_name", "is_staff")
